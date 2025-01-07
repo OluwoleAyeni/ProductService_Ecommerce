eb9f420dd56ce077a36764fdb8acc8f0cd038aa9
Wole Ayeni <94427750+OluwoleAyeni@users.noreply.github.com>
2025-01-07 12:37:50 +0100

    updating the project folder

--- src/domain/entities/product.js
+++ src/domain/entities/product.js
@@ -1,17 +1,19 @@
 class product {
-    constructor({ id, name, price, description }) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.description = description;
+  constructor({ id, name, price, description }) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.description = description;
 
-        this.validate();
-    }
+    this.validate();
+  }
 
-    validate() {
-        if (!this.name || this.name.trim() === '') throw new Error('Product name requires a string');
-        if (this.price <= 0) throw new Error('Product price must be greater that zero');
-    }
+  validate() {
+    if (!this.name || this.name.trim() === "")
+      throw new Error("Product name requires a string");
+    if (this.price <= 0)
+      throw new Error("Product price must be greater that zero");
+  }
 }
 
-module.exports = product; 
+module.exports = product;
