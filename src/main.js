2a650294dfd3b48443192ebd57468d603b4476b2
Wole Ayeni <94427750+OluwoleAyeni@users.noreply.github.com>
2025-01-12 12:51:28 +0100

    upadte

--- src/main.js
+++ src/main.js
@@ -5,3 +5,27 @@ const DataRepository = require("/Framework/Database/dataRepository");
 const addProductUsecase = require("../Usecases/addProductUsecase");
 const getAllProductsUsecase = require("../Usecases/GetAllProductUsecase");
 const getProductbyIdUsecase = require("../Usecases/getProductbyIdUsecase");
+const ProductRoutes = require("../web/routes");
+
+(async () => {
+  // Connect to MongoDB
+  await connectDB();
+
+  // Dependency Injection
+  const productRepository = new DataRepository(ProductModel);
+  const useCases = {
+    addProductUseCase: new addProductUsecase(productRepository),
+    getAllProductsUseCase: new getAllProductsUsecase(productRepository),
+    getProductByIdUseCase: new getProductbyIdUsecase(productRepository),
+  };
+
+  // Initialize Express App
+  const app = express();
+  app.use(express.json());
+  app.use("/api", ProductRoutes);
+
+  // Start Server
+  app.listen(3000, () =>
+    console.log("E-commerce API running on http://localhost:3000")
+  );
+})();
